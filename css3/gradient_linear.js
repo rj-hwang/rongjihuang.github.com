e0b0d51c7f4a4231a53a83004f74666793b205b4
rongjihuang <rongjihuang@gmail.com>
2012-09-29 00:16:28 +0800

    finished change color and number

--- css3/gradient_linear.js
+++ css3/gradient_linear.js
@@ -1,25 +1,85 @@
-jQuery(function($) {
-	var $trs = $("#items>tbody>tr");
-
-	// find the real css config code. {cssKey: cssValue, ...}
-	function getRealCSSConfig(){
+var cssCfg = {
+	/** get the real css config code. return {cssKey: cssValue, ...} */
+	getRealCSSConfig: function($container){
+		// find all config line
+		var $lines = $container.find(".line");
 		var css = {};
-		$trs.each(function(){
+		$lines.each(function(){
 			var $this = $(this);
-			var $radio = $this.find("input:radio");
-			if ($radio.size() == 0 || $radio[0].checked) {
-				css[$.trim($this.find("td.label").text())] = $.trim($this.find("td.value").text().replace(/^:|;$/g,""));
+
+			var $select = $this.find("input:radio,input:checkbox");
+			if ($select.size() == 0 || $select[0].checked) {
+				css[$.trim($this.find(".key").text())] = $.trim($this.find(".value").text().replace(/^:|;$/g,""));
 			}
 		});
 		return css;
-	}
+	},
 
-	// show the real css code
-	var css = getRealCSSConfig();
-	var code = ["Real CSS code {"];
-	for(var k in css){
-		code.push("    " + k + ": " + css[k] + ";");
+	/** update */
+	doChange: function(){
+		console.log("doChange");
+		var $lines = $("#lines");
+		var css = cssCfg.getRealCSSConfig($lines);
+		var code = ["#target {"];
+		for(var k in css){
+			code.push("    " + k + ": " + css[k] + ";");
+		}
+		code.push("}");
+		$("#code").html(code.join("\r\n"));
+
+		// set target css
+		$("#target").css(css);
 	}
-	code.push("}");
-	$("#code").html(code.join("\r\n"));
+};
+
+jQuery(function($) {
+	// show the real css code
+	cssCfg.doChange();
+
+	var $lines = $("#lines");
+	$lines.find(".line>td.value>div").each(function() {
+		var $this = $(this);
+		var cfg = $this.data("cfg");
+		console.log("cfg.type=" + cfg.type);
+
+		if (cfg.type == "color") {	// select color
+			var $picker = $('<div class="picker color"/>').appendTo("body")
+				.mousedown(function(e) {
+					e.stopPropagation();
+				});
+			var farb = $.farbtastic($picker, function(c) {
+				if (c) {
+					$this.text(c.toUpperCase()).next().css({backgroundColor:c.toUpperCase()});
+					cssCfg.doChange();
+				}
+			});
+			$this.click(function() {
+				farb.setColor($this.text());
+				$picker.css($this.offset()).fadeIn();
+				$(document).one('mousedown', function handler() {
+					$picker.fadeOut();
+				});
+			});
+			
+			$this.next().css({backgroundColor:$this.text()});
+		} else if (cfg.type == "number") {	// change number
+			var $picker = $('<div class="picker number"><input type="range" value="0" max="100" step="1"/></div>').appendTo("body")
+				.mousedown(function(e) {
+					e.stopPropagation();
+				});
+			$picker.children().change(function() {
+				$this.text(this.value);
+				cssCfg.doChange();
+			});
+			$this.click(function() {
+				$picker.children().attr({min: cfg.min, max: cfg.max}).val($this.text());
+				$picker.css($this.offset()).fadeIn();
+				$(document).one('mousedown', function handler() {
+					$picker.fadeOut();
+				});
+			});
+		}
+	});
+
+	$lines.find("input:radio,input:checkbox").change(cssCfg.doChange);
 });
